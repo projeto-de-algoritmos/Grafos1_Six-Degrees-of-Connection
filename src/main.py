53e519a7f7c308f26acf723662c19902ae5d11b1
DaviMatheus <matdavimat@gmail.com>
2023-05-01 20:39:20 -0300

    fixing_bug

--- src/main.py
+++ src/main.py
@@ -10,9 +10,9 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename)) 
         
-nodes = pd.read_csv('../dataset/nodes.csv')
-edges = pd.read_csv('../dataset/edges.csv')
-hero_network = pd.read_csv('../dataset/hero-network.csv')
+nodes = pd.read_csv('./dataset/nodes.csv')
+edges = pd.read_csv('./dataset/edges.csv')
+hero_network = pd.read_csv('./dataset/hero-network.csv')
 print(nodes.head())
 print(edges.head())
 print(hero_network.head())
@@ -27,4 +27,4 @@ Subset = pd.concat([Thor,Cap,IronMan],axis = 0)
 G=nx.from_pandas_edgelist(Subset, 'hero1', 'hero2')
 plt.figure(figsize = (20,20))
 nx.draw(G, with_labels=True, node_size=8, node_color='red', edge_color='blue')
-plt.show(block=False)
+plt.show(block=True)
